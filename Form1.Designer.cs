4a748fe4c3f7c6962575227eca2914cdf0c25114
VictorHMarcondes <108373750+VictorHMarcondes@users.noreply.github.com>
2022-06-28 21:51:15 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace AtividadeArea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.btnCalular = new System.Windows.Forms.Button();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(64, 105);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 0;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Location = new System.Drawing.Point(67, 135);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(31, 13);
+            this.lblBase.TabIndex = 1;
+            this.lblBase.Text = "Base";
+            // 
+            // btnCalular
+            // 
+            this.btnCalular.Location = new System.Drawing.Point(102, 157);
+            this.btnCalular.Name = "btnCalular";
+            this.btnCalular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalular.TabIndex = 2;
+            this.btnCalular.Text = "Calcular";
+            this.btnCalular.UseVisualStyleBackColor = true;
+            this.btnCalular.Click += new System.EventHandler(this.btnCalular_Click);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(102, 102);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 3;
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(102, 128);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBase.TabIndex = 4;
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(70, 194);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(29, 13);
+            this.lblArea.TabIndex = 5;
+            this.lblArea.Text = "Área";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(105, 194);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 6;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(62, 47);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(351, 25);
+            this.lblTitulo.TabIndex = 7;
+            this.lblTitulo.Text = "CALCULAR AREA DO TRIÂNGULO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.btnCalular);
+            this.Controls.Add(this.lblBase);
+            this.Controls.Add(this.lblAltura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Button btnCalular;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
